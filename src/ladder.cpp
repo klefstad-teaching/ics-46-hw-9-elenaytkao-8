a864a9d6fcc78f0f21fe66ebc8ccf2e2e7f3f641
Elena Yi Ting Kao <elenakao940808@gmail.com>
2025-03-15 11:19:42 +0000

    test4

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <cassert>
 using namespace std;
 
 void error(string w1, string w2, string m) {
@@ -82,17 +83,17 @@ void verify_word_ladder() {
 
     load_words(word_list, "words.txt");
 
-    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+    assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
 
-    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+    assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
 
-    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+    assert(generate_word_ladder("code", "data", word_list).size() == 6);
 
-    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+    assert(generate_word_ladder("work", "play", word_list).size() == 6);
 
-    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+    assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
 
-    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+    assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
 
 }
 
