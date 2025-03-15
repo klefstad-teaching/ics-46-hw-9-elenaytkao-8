24a3ac9a81c4cb48ba4918c9bec05bd0cdc9b417
Elena Yi Ting Kao <elenakao940808@gmail.com>
2025-03-15 11:39:15 +0000

    test7

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
-#include <cassert>
+#define my_assert(e) (cout << #e << ((e) ? " passed" : " failed") << endl)
 using namespace std;
 
 void error(string w1, string w2, string m) {
@@ -76,7 +76,7 @@ void print_word_ladder(const vector<string> &ladder) {
     }
     cout << " \n";
 }
-my_assert(e) {cout << e << ((e) ? " passed": " failed") << endl;}
+
 void verify_word_ladder() {
 
     set<string> word_list;
